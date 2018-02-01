f49c71fb1657610330ff61957e432cac719c3f84
CaroAmarillo <caro_amarillo@hotmail.com>
2018-02-01 11:36:14 -0300

    agrego ej

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/13/d0d60df05a070018156fdb610027fee1
@@ -0,0 +1,15 @@
+package com.ejercicio17;
+
+public class Password {
+	
+	public int longitud;
+	public String contraseña;
+	public int longitudPorDefecto = 8;
+	
+	public Password(){}
+	
+	public Password(int longitud){
+		this.longitud = longitud;
+	}
+	
+}
--- Ejercicio17/src/com/ejercicio17/Password.java
+++ Ejercicio17/src/com/ejercicio17/Password.java
@@ -3,6 +3,8 @@ package com.ejercicio17;
 public class Password {
 	
 	public int longitud;
+	
+
 	public String contraseña;
 	public int longitudPorDefecto = 8;
 	
@@ -10,6 +12,62 @@ public class Password {
 	
 	public Password(int longitud){
 		this.longitud = longitud;
+		this.contraseña = generarPassword();
+	}
+
+	public int getLongitud() {
+		return longitud;
+	}
+	public void setLongitud(int longitud) {
+		this.longitud = longitud;
+	}
+
+	public String getContraseña() {
+		return contraseña;
 	}
 	
+	public String generarPassword(){
+		String password="";
+        for (int i=0;i<longitud;i++){
+            int pedir=((int)Math.floor(Math.random()*3+1));
+  
+            if (pedir==1){
+                char letraMinuscula=(char)((int)Math.floor(Math.random()*(123-97)+97));
+                password+=letraMinuscula;
+            }else{
+                if(pedir==2){
+                    char letraMayuscula=(char)((int)Math.floor(Math.random()*(91-65)+65));
+                    password+=letraMayuscula;
+                }else{
+                    char numeros=(char)((int)Math.floor(Math.random()*(58-48)+48));
+                    password+=numeros;
+                }
+            }
+        }
+        return password;
+    }
+	
+	public boolean esFuerte(){
+        int numeros=0;
+        int minusculas=0;
+        int mayusculas=0;
+        for (int i=0;i<contraseña.length();i++){
+                if (contraseña.charAt(i)>=97 && contraseña.charAt(i)<=122){
+                	minusculas+=1;
+                }else{
+                    if (contraseña.charAt(i)>=65 && contraseña.charAt(i)<=90){
+                    	mayusculas+=1;
+                }else{
+                	numeros+=1;
+                    }
+                }
+            }
+            if (numeros>=5 && minusculas>=1 && mayusculas>=2){
+            return true;
+        }else{
+            return false;
+        }
+    }
+  
+		
 }
