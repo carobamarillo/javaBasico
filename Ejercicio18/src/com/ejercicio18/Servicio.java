41e4a1fa8351e694d4f8feb3809e5575b4660667
CaroAmarillo <caro_amarillo@hotmail.com>
2018-02-01 14:24:38 -0300

    agrego ej

--- Ejercicio18/src/com/ejercicio18/Servicio.java
+++ Ejercicio18/src/com/ejercicio18/Servicio.java
@@ -8,27 +8,11 @@ public class Servicio extends Producto{
 		}
 
 		@Override
-		public void descontarStock() {
-			//no hace nada xq no tiene stock los servicios rulo		
-		}
-
+		public int getStock() {return 1;}
 		@Override
-		public int getStock() {
-			//si existe siempre hay stock 
-			return 1;
-		}
-	//
-//		@Override
-//		public int sumarStock(int cantidad, int cantMaterial) {
-//			System.out.println("Esto es un servicio no se puede agregar Stock");
-//			return cantMaterial;
-//			
-//		}
-
+		public String toString() {return super.nombre;}
 		@Override
-		public String toString() {
-			return super.nombre;
-		}
+		public void descontarStock() {}
 
 		
 	}
