d3b17884ffda364f89470b6ede99c40d2e066916
CaroAmarillo <caro_amarillo@hotmail.com>
2018-02-07 16:58:47 -0300

    agrego ej

---
+++ Receta/src/main/java/com/ManyToMany/Receta/RecetaApplication.java
@@ -0,0 +1,12 @@
+package com.ManyToMany.Receta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecetaApplication.class, args);
+	}
+}
